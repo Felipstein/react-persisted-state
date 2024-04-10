d067059a805c1113cdf938859ae88d16bcca142b
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-04-10 12:54:30 -0300

    build: improves build spec

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
+import path from 'path';
+
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, 'lib/usePersistedState.ts'),
+      formats: ['es', 'cjs'],
+    },
+  },
 });
