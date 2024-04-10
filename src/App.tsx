fe46faabe67b697cb4d4d4a09f5af9fde2268577
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-04-10 13:24:13 -0300

    style(fix): apply prettier plugin tailwindcss rules

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ export function App() {
   );
 
   return (
-    <div className="bg-zinc-950 text-white grid place-items-center h-full">
+    <div className="grid h-full place-items-center bg-zinc-950 text-white">
       <div className="w-[150px] space-y-3">
         <div className="flex items-center gap-2">
           <p>Loading:</p>
 
           <span
             data-loading={isLoading}
-            className="font-medium text-lg data-[loading=true]:text-sky-400 data-[loading=false]:text-amber-400 flex items-center"
+            className="flex items-center text-lg font-medium data-[loading=false]:text-amber-400 data-[loading=true]:text-sky-400"
           >
             {isLoading ? 'true' : 'false'}
 
@@ -32,7 +32,7 @@ export function App() {
           <button
             type="button"
             data-loading={isLoading}
-            className="w-full bg-teal-900 data-[loading=true]:bg-red-900 font-medium rounded-md px-4 py-1.5 hover:bg-teal-800 data-[loading=true]:hover:bg-red-800 transition-colors"
+            className="w-full rounded-md bg-teal-900 px-4 py-1.5 font-medium transition-colors hover:bg-teal-800 data-[loading=true]:bg-red-900 data-[loading=true]:hover:bg-red-800"
             onClick={() => setIsLoading((state) => !state)}
           >
             {isLoading ? 'Stop' : 'Start'}
@@ -40,7 +40,7 @@ export function App() {
 
           <button
             type="button"
-            className="w-full bg-amber-900 font-medium rounded-md px-4 py-1.5 hover:bg-amber-800 transition-colors"
+            className="w-full rounded-md bg-amber-900 px-4 py-1.5 font-medium transition-colors hover:bg-amber-800"
             onClick={removeLoadingState}
           >
             Remove Persisted
