825c0834d24d58f9bff2c4ce7687b42d13d085bc
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-04-10 12:56:14 -0300

    build: ignore test files

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [react(), dts({ include: ['lib'], exclude: '**/*.test.ts' })],
   build: {
     copyPublicDir: false,
     lib: {
